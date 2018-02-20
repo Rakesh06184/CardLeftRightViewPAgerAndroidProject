55b2ac460ccb8ac698c62837cbf2b7f53f4483a2
Rakesh06184 <2702rakesh@gmail.com>
2018-02-20 19:04:05 +0530

    Android Card view Left and Right View pager

---
+++ app/src/main/java/com/github/rubensousa/viewpagercards/MainActivity.java
@@ -0,0 +1,55 @@
+package com.github.rubensousa.viewpagercards;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+
+public class MainActivity extends AppCompatActivity  {
+
+    private Button mButton;
+    private ViewPager mViewPager;
+
+    private CardPagerAdapter mCardAdapter;
+    private ShadowTransformer mCardShadowTransformer;
+   // private CardFragmentPagerAdapter mFragmentCardAdapter;
+  //  private ShadowTransformer mFragmentCardShadowTransformer;
+
+    private boolean mShowingFragments = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mViewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        mCardAdapter = new CardPagerAdapter();
+
+
+        mCardAdapter.addCardItem(new CardItem(R.string.title_1, R.string.text_1));
+        mCardAdapter.addCardItem(new CardItem(R.string.title_2, R.string.text_1));
+
+
+       /* mFragmentCardAdapter = new CardFragmentPagerAdapter(getSupportFragmentManager(),
+                dpToPixels(2, this));*/
+
+
+
+
+        mCardShadowTransformer = new ShadowTransformer(mViewPager, mCardAdapter);
+      //  mFragmentCardShadowTransformer = new ShadowTransformer(mViewPager, mFragmentCardAdapter);
+
+        mViewPager.setAdapter(mCardAdapter);
+        mViewPager.setPageTransformer(false, mCardShadowTransformer);
+        mViewPager.setOffscreenPageLimit(3);
+    }
+
+    public static float dpToPixels(int dp, Context context) {
+        return dp * (context.getResources().getDisplayMetrics().density);
+    }
+
+}
